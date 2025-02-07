e195c79f40254dd741d879d4017c16706d0efdfe
eriksun0310 <113155520+eriksun0310@users.noreply.github.com>
2025-02-07 13:42:49 +0800

    處理 複製 剪下的邏輯到一半

--- src/components/Workspace.tsx
+++ src/components/Workspace.tsx
@@ -4,14 +4,21 @@ import {
   IconEdit,
   IconTrash,
   IconInfoCircle,
+  IconCut,
+  IconCopy,
+  IconClipboard,
 } from "@tabler/icons-react";
 import { useFileManager } from "../context/FileManagerContext";
 import Path from "./Path";
-import { useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { Menu, TextInput } from "@mantine/core";
 import { FileType } from "../type";
 import CreateNew from "./CreateNew";
 import InfoModal from "./InfoModal";
+
+type TempFile = FileType & {
+  isCut: boolean;
+};
 
 const Workspace = () => {
   const { files, setFiles, currentFolder, setCurrentFolder, viewStyle } =
@@ -40,6 +47,12 @@ const Workspace = () => {
   // 搜尋文字
   const [searchText, setSearchText] = useState<string>("");
 
+  // 暫存(複製/貼上)的檔案
+  const [tempFile, setTempFile] = useState<TempFile | null>();
+
+  // 紀錄 滑鼠 hover 的檔案
+  // const [hoveredFile, setHoveredFile] = useState<FileType | null>(null);
+
   const renderFiles = useMemo(() => {
     return files?.filter(
       (f) => f.id !== "0" && f.parentId === currentFolder.id
@@ -54,7 +67,10 @@ const Workspace = () => {
     e: React.MouseEvent;
     file: FileType;
   }) => {
+    // 阻止點擊事件
     e.preventDefault();
+
+    setSelectedFile(file);
 
     // 找到點擊的 `div`（這裡用 e.currentTarget 指向被點擊的區塊）
     const targetElement = e.currentTarget as HTMLElement;
@@ -85,6 +101,7 @@ const Workspace = () => {
 
   // 模擬儲存新檔案名稱的動作，這裡可以做 API 請求或直接更新 state
   const saveRename = (file: FileType) => {
+    console.log("currentFolder", currentFolder);
     console.log("Saving new name:", inputValue, "for file", file);
 
     // 如果 inputValue 為空，則使用原始檔案名稱
@@ -115,6 +132,78 @@ const Workspace = () => {
     setFiles(newFiles);
   };
 
+  // 複製剪下事件(存在 tempFile 中)
+  const copyOrCutEvent = ({
+    file,
+    isCut,
+  }: {
+    file: FileType;
+    isCut: boolean;
+  }) => {
+    if (!file) return;
+
+    const id = isCut ? file.id : Date.now().toString();
+    const newName = isCut ? file.name : file.name + "-複製";
+
+    const newFile = {
+      ...file,
+      id: id,
+      name: newName,
+      parentId: currentFolder.id,
+
+      path: file.isDir
+        ? currentFolder.id === "0"
+          ? `/${newName}`
+          : `${currentFolder.path}/${newName}`
+        : currentFolder.path,
+    };
+
+    console.log("newFile", newFile);
+    setTempFile({ ...newFile, isCut });
+  };
+
+  console.log("tempFile", tempFile);
+
+  // 貼上事件
+  const pasteEvent = (tempFile: TempFile) => {
+    console.log("pasteEvent tempFile", tempFile);
+    if (!tempFile) return;
+    setFiles((prevFiles) => [...prevFiles, tempFile]);
+  };
+
+  useEffect(() => {
+    const handleKeyDown = (e: KeyboardEvent) => {
+      if (
+        e.ctrlKey &&
+        (e.key.toLowerCase() === "c" || e.key.toLowerCase() === "x")
+      ) {
+        if (!selectedFile) return; // 沒有選擇檔案的話，不做任何動作
+
+        if (e.ctrlKey && e.key.toLowerCase() === "c") {
+          copyOrCutEvent({
+            file: selectedFile!,
+            isCut: false,
+          });
+        } else if (e.ctrlKey && e.key.toLowerCase() === "x") {
+          setFiles((prevFiles) =>
+            prevFiles.filter((f) => f.id !== selectedFile!.id)
+          );
+
+          copyOrCutEvent({
+            file: selectedFile!,
+            isCut: true,
+          });
+        }
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, [selectedFile]);
+
   return (
     <>
       {/* 檔案資訊modal */}
@@ -127,10 +216,32 @@ const Workspace = () => {
       <div className="w-[85%] h-full">
         <Path searchText={searchText} setSearchText={setSearchText} />
 
-        <div className={viewStyle === "grid" ? "flex flex-wrap" : ""}>
+        <div
+          className={`${viewStyle === "grid" ? "flex flex-wrap" : ""} h-full`}
+          // className={viewStyle === "grid" ? "flex flex-wrap h-full" : "h-full"}
+          tabIndex={0} // 讓 div 可接收鍵盤事件
+          onClick={(e) => {
+            // 檢查點擊的元素是否是檔案元素,如果不是取消選取檔案
+            if (
+              !(e.target as HTMLElement).closest(".file") &&
+              !(e.target as HTMLElement).closest(".menuDropdown")
+            ) {
+              console.log("取消選取檔案");
+              setSelectedFile(null);
+            }
+          }}
+          onKeyDown={(e) => {
+            console.log("e", e);
+            // 在任一處按下 ctrl + v
+            if (e.ctrlKey && e.key.toLowerCase() === "v") {
+              console.log("tempFile", tempFile);
+              pasteEvent(tempFile);
+            }
+          }}
+        >
           {renderFiles.map((file) => {
             return (
-              <div key={file.id}>
+              <div key={file.id} className="menu">
                 <Menu
                   position="right-start"
                   withArrow
@@ -146,18 +257,24 @@ const Workspace = () => {
                       }}
                       className={`${viewStyle === "grid" ? "flex-col" : ""} flex items-center m-2 cursor-pointer relative file`}
                       key={file.id}
+                      // 單擊選取檔案
                       onClick={() => {
+                        setSelectedFile(file);
+                      }}
+                      // 雙擊進入資料夾
+                      onDoubleClick={() => {
                         if (file.isDir) {
                           setCurrentFolder(file);
                         }
                       }}
+                      // 點右鍵顯示右鍵選單
                       onContextMenu={(e) =>
                         clickRightEvent({
                           e,
                           file,
                         })
                       }
-                      {...(menuOpenId === file.id
+                      {...(selectedFile?.id === file.id
                         ? { "data-active": true }
                         : {})}
                     >
@@ -182,11 +299,11 @@ const Workspace = () => {
                           onChange={(event) => {
                             setInputValue(event.currentTarget.value);
                           }}
-                          // 當 TextInput 失去焦點時保存變更
+                          // 當 TextInput 失去焦點時保存變更 檔案名稱
                           onBlur={() => saveRename(file)}
-                          // 當按下 Enter 鍵時也儲存變更
-                          onKeyDown={(event) => {
-                            if (event.key === "Enter") {
+                          // 當按下 Enter 鍵時也儲存變更 檔案名稱
+                          onKeyDown={(e) => {
+                            if (e.key === "Enter") {
                               saveRename(file);
                             }
                           }}
@@ -199,7 +316,7 @@ const Workspace = () => {
                   </Menu.Target>
 
                   <Menu.Dropdown
-                    className="absolute"
+                    className="absolute menuDropdown"
                     style={{
                       top: menuPosition?.top ?? 0, // 如果 menuPosition 存在則使用該座標
                       left: menuPosition?.left ?? 0,
@@ -211,6 +328,37 @@ const Workspace = () => {
                     >
                       詳細資訊
                     </Menu.Item>
+                    <Menu.Item
+                      leftSection={<IconCut stroke={1.25} size={18} />}
+                      onClick={() =>
+                        copyOrCutEvent({
+                          file,
+                          isCut: true,
+                        })
+                      }
+                    >
+                      剪下
+                    </Menu.Item>
+                    <Menu.Item
+                      leftSection={<IconCopy stroke={1.25} size={18} />}
+                      onClick={() =>
+                        copyOrCutEvent({
+                          file,
+                          isCut: false,
+                        })
+                      }
+                    >
+                      複製
+                    </Menu.Item>
+
+                    <Menu.Item
+                      leftSection={<IconClipboard stroke={1.25} size={18} />}
+                      onClick={() => infoEvent(file)}
+                      disabled={!tempFile}
+                    >
+                      貼上
+                    </Menu.Item>
+
                     <Menu.Item
                       leftSection={<IconEdit stroke={1.25} size={18} />}
                       onClick={() => reNameEvent(file)}
