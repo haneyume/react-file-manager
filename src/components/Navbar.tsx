f4fd5ee272b7640ecb5d31ac6b4d7d7341aaf691
eriksun0310 <113155520+eriksun0310@users.noreply.github.com>
2025-02-12 17:28:24 +0800

    transformedTree 到一半

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -7,6 +7,41 @@ const Navbar = () => {
   const initialFolder = useMemo(() => {
     return files.filter((f) => f.isDir && f.parentId === "0");
   }, [files]);
+
+  const fs = useMemo(() => {
+    return files.filter((f) => f.id !== "0");
+  }, [files]);
+
+  console.log("fs", fs);
+
+  const transformedTree = useMemo(() => {
+    const tree = fs.reduce((acc, file) => {
+      if (file.isDir && file.parentId === "0") {
+        acc[file.id] = file;
+      }
+      return acc;
+    }, {});
+
+    console.log("tree", tree);
+    fs.forEach((file) => {
+      console.log("file 1111", file);
+      if (file.isDir && file.parentId !== "0") {
+        console.log("file 22222", file);
+        const parent = tree[file?.parentId];
+        if (!parent?.children) {
+          parent.children = [];
+        }
+        // 不存在的話就加入
+        if (!parent.children.some((f) => f.id === file.id)) {
+          parent.children.push(file);
+        }
+      }
+    });
+
+    return tree;
+  }, [files]);
+
+  console.log("transformedTree", transformedTree);
   return (
     <div className="w-[15%] bg-[#f9fafc] h-full min-w-[200px] ">
       <div
